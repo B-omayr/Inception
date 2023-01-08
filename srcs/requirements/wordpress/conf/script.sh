501dac13387edcb8b27d84a1396ef463cd3266dd
B-omayr <iomayr@student.1337.ma>
2023-01-08 12:03:50 +0100

    update: use env variable instead of using it manually

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -4,8 +4,7 @@
 
 wp core download --path=/var/www/html --allow-root
 rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASS --dbhost=$HOST --path=/var/www/html/ --allow-root --skip-check --extra-php <<PHP
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASS --dbhost=$DB_HOST --path=/var/www/html/ --allow-root --skip-check --extra-php <<PHP
 define('WP_CACHE', true);
 define('WP_REDIS_HOST', 'redis');
 define( 'WP_REDIS_PORT', 6379 );
@@ -14,14 +13,15 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 PHP
 
-wp core install --url=iomayr.42.fr --title=Example --admin_user=ibra --admin_password=123456 --admin_email=ibra@gmail.com --path=/var/www/html/ --allow-root
-wp user create brahim user@gmail.com --role=author --user_pass=123456 --allow-root --path=/var/www/html/
+wp core install --url=$URL --title=$Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAI --path=/var/www/html/ --allow-root
+wp user create brahim user@gmail.com --role=author --user_pass=$WP_USER_PASS --allow-root --path=/var/www/html/
 
 # the file /var/run/php/ will be created automatically when you run php service
 service php7.3-fpm start
 wp plugin install redis-cache --path=/var/www/html --activate --allow-root 
 wp redis enable --path=/var/www/html/ --allow-root
 service php7.3-fpm stop
-# tail -f /dev/null
+chown -R www-data:www-data -f /var/www/html 
+
 # /usr/sbin/php-fpm;
 php-fpm7.3 -F
