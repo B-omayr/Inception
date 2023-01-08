48d27371a6be83f359f69d9450eecf437911c74d
B-omayr <iomayr@student.1337.ma>
2023-01-08 11:59:42 +0100

    fix duplicating problem for the creating of user

--- srcs/requirements/mariadb/conf/initial_db.sql
+++ srcs/requirements/mariadb/conf/initial_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'iomayr'@localhost;
-GRANT ALL PRIVILEGES ON wordpress.* to 'iomayr'@'%' IDENTIFIED BY 'user123321';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root123321';
+CREATE USER IF NOT EXISTS 'iomayr' IDENTIFIED BY 'user42';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'iomayr'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root42';
 FLUSH PRIVILEGES;
