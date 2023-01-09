b55514a498c53fbe08701ec4411185ca2fa62ac5
B-omayr <iomayr@student.1337.ma>
2023-01-09 09:00:45 +0100

    fix: change the name for variable env

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Configure WordPress
-
 wp core download --path=/var/www/html --allow-root
 rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASS --dbhost=$DB_HOST --path=/var/www/html/ --allow-root --skip-check --extra-php <<PHP
@@ -13,7 +12,7 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 PHP
 
-wp core install --url=$URL --title=$Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAI --path=/var/www/html/ --allow-root
+wp core install --url=$URL --title=$TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html/ --allow-root
 wp user create brahim user@gmail.com --role=author --user_pass=$WP_USER_PASS --allow-root --path=/var/www/html/
 
 # the file /var/run/php/ will be created automatically when you run php service
